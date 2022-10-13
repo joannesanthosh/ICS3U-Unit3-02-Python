8f35144f9e979cb159540c3e68a013d4d346f8e4
Joanne Santhosh <joanne.santhosh@mths.ca>
2022-10-13 15:24:46 +0000

    update

---
+++ constants.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Created by: Joanne Santhosh
+# Created on: Oct 2022
+# This file holds the constants
+
+GUESSING_NUMBER = 5
--- guess_the_number.py
+++ guess_the_number.py
@@ -4,19 +4,21 @@
 # Created on: Oct 2022
 # This program gets the user to guess a number correctly or incorrectly
 
+import constants
+
 
 def main():
-    # this function checks if the guess is correct 
+    # this function checks if the guess is correct
 
     # input
     user_guess = int(input("Enter a number between 0-9: "))
     print("")
 
     # process & output
-    if user_guess != 5:
+    if user_guess != constants.GUESSING_NUMBER:
         print("Your guess is incorrect. Try again!")
     
-    if user_guess == 5:
+    if user_guess == constants.GUESSING_NUMBER:
         print("Your guess is correct!")
 
         print("\nDone.")
