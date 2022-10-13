1390b5b73b8bfd2654a38713d7dc52ea2cc5e9a9
Joanne Santhosh <joanne.santhosh@mths.ca>
2022-10-13 14:47:14 +0000

    init

---
+++ guess_the_number.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Created by: Joanne Santhosh
+# Created on: Oct 2022
+# This program gets the user to guess a number correctly or incorrectly
+
+
+def main():
+    # this function checks if the guess is correct 
+
+    # input
+    user_guess = int(input("Enter a number between 0-9: "))
+    print("")
+
+    # process & output
+    if user_guess != 5:
+        print("Your guess is incorrect. Try again!")
+    
+    if user_guess == 5:
+        print("Your guess is correct!")
+
+        print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
